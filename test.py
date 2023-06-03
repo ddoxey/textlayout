025188884ff6c106dcb550807dfe9ecc67190a56
Dylan Doxey <dylan.doxey@gmail.com>
2023-06-03 14:30:40 -0700

    Added more tests

--- test.py
+++ test.py
@@ -1,36 +1,146 @@
 #!/usr/bin/env python3
+"""
+    Unit tests for the TextGrid and TextLayout classes.
+
+    Author: Dylan Doxey <dylan.doxey@gmail.com>
+    Date: May 29, 2023
+"""
 import unittest
 from textlayout import TextLayout, TextGrid
 
 
 class TestTextLayout(unittest.TestCase):
+    """These tests exercise and demonstrate the
+       TextGrid and TextLayout behaviors.
+    """
 
     def test_grid_emtpy1x1(self):
+        """This test demonstrates creating a blank 1 by 1 grid."""
         grid = TextGrid(1, 1)
         lines = str(grid).split("\n")
         expect = [' ']
         self.assertEqual(lines, expect)
 
-    def test_grid_emtpy3x3_border(self):
-        grid = TextGrid(3, 3, border=True)
+    def test_grid_emtpy3x3(self):
+        """This test demonstrates creating a blank 3 by 3 grid."""
+        grid = TextGrid(3, 3)
         lines = str(grid).split("\n")
         expect = ['   ',
                   '   ',
                   '   ']
         self.assertEqual(lines, expect)
 
+    def test_grid_setstr3x3(self):
+        """This test demonstrates setting the value of a 3 by 3 grid using
+           a string of 4 four character strings separated by line breaks
+           the source data.
+        """
+        grid = TextGrid(3, 3)
+        grid.set("abcd\nefgh\nijkl\nmno")
+        lines = str(grid).split("\n")
+        expect = ['abc',
+                  'efg',
+                  'ijk']
+        self.assertEqual(lines, expect)
+
+    def test_grid_setlist3x3(self):
+        """This test demonstrates setting the value of a 3 by 3 grid using
+           a list of 4 four character strings as the source data.
+        """
+        grid = TextGrid(3, 3)
+        grid.set(['abcd',
+                  'efgh',
+                  'ijkl'
+                  'lmno'])
+        lines = str(grid).split("\n")
+        expect = ['abc',
+                  'efg',
+                  'ijk']
+        self.assertEqual(lines, expect)
+
+    def test_grid_setlargergrid3x3(self):
+        """This test demonstrates setting the value of a 3 by 3 grid using
+           a 4 x 4 grid as the source data.
+        """
+        grid = TextGrid(3, 3)
+        grid4x4 = TextGrid(4, 4)
+        grid4x4.set(['abcd',
+                     'efgh',
+                     'ijkl'
+                     'lmno'])
+        grid.set(grid4x4)
+        lines = str(grid).split("\n")
+        expect = ['abc',
+                  'efg',
+                  'ijk']
+        self.assertEqual(lines, expect)
+
+    def test_grid_setsmallergrid3x3(self):
+        """This test demonstrates setting the value of a 3 by 3 grid using
+           a 2 x 2 grid as the source data.
+        """
+        grid = TextGrid(3, 3)
+        grid2x2 = TextGrid(2, 2)
+        grid2x2.set(['ab',
+                     'ef'])
+        grid.set(grid2x2)
+        lines = str(grid).split("\n")
+        expect = ['ab ',
+                  'ef ',
+                  '   ']
+        self.assertEqual(lines, expect)
+
+    def test_grid_nonemtpy3x3_border(self):
+        """This test demonstrates a text grid with border renders as
+           a 3 x 3 grid with one character of text on stringification.
+        """
+        grid = TextGrid(3, 3, border=True)
+        grid.write('aa')
+        lines = str(grid).split("\n")
+        expect = ['   ',
+                  ' a ',
+                  '   ']
+        self.assertEqual(lines, expect)
+
     def test_layout_emtpy1x1(self):
+        """This test demonstrates that a new one character layout with
+           no text grids added will show zero reservations and no text
+           on stringification.
+        """
         layout = TextLayout(1, 1)
         lines = layout.reservations().split("\n")
         expect = ['+---+',
                   '| 0 |',
                   '+---+']
         self.assertEqual(lines, expect)
-        lines = str(layout).split("\n")
         expect = [' ']
+        self.assertEqual(layout.lines, expect)
+
+    def test_layout_bordered1x1(self):
+        """This test demonstrates a 3 by 3 layout with
+           a bordered 1 by 1 grid strififies correctly.
+        """
+        layout = TextLayout(3, 3)
+        grid = TextGrid(3, 3, border=True)
+        self.assertTrue(layout.add(grid))
+        grid.set('a')
+        lines = layout.reservations().split("\n")
+        expect = ['+-------+',
+                  f'| {grid.gid} {grid.gid} {grid.gid} |',
+                  f'| {grid.gid} {grid.gid} {grid.gid} |',
+                  f'| {grid.gid} {grid.gid} {grid.gid} |',
+                  '+-------+']
         self.assertEqual(lines, expect)
+        expect = ['   ',
+                  ' a ',
+                  '   ']
+        self.assertEqual(layout.lines, expect)
 
     def test_layout_emtpy2x2(self):
+        """This test demonstrates that a new 2 by 2 layout with
+           no text grids added will show zero reservations and
+           each position has no text on stringification.
+        """
         layout = TextLayout(2, 2)
         lines = layout.reservations().split("\n")
         expect = ['+-----+',
@@ -38,64 +148,90 @@ class TestTextLayout(unittest.TestCase):
                   '| 0 0 |',
                   '+-----+']
         self.assertEqual(lines, expect)
-        lines = str(layout).split("\n")
         expect = ['  ',
                   '  ']
-        self.assertEqual(lines, expect)
+        self.assertEqual(layout.lines, expect)
 
     def test_layout_2x2addblank4x1x1(self):
+        """This test demonstrates that the text grids are added to
+           to the layout in a left to right, top to bottom, sequence.
+           The positions of each grid is verified after being added
+           to the layout.
+        """
         layout = TextLayout(2, 2)
         grid0x0 = TextGrid(1, 1)
         grid0x1 = TextGrid(1, 1)
         grid1x0 = TextGrid(1, 1)
         grid1x1 = TextGrid(1, 1)
-        layout.add(grid0x0)
-        layout.add(grid0x1)
-        layout.add(grid1x0)
-        layout.add(grid1x1)
+        self.assertTrue(layout.add(grid0x0))
+        self.assertTrue(layout.add(grid0x1))
+        self.assertTrue(layout.add(grid1x0))
+        self.assertTrue(layout.add(grid1x1))
         lines = layout.reservations().split("\n")
         expect = ['+-----+',
-                  f'| {grid0x0.id} {grid0x1.id} |',
-                  f'| {grid1x0.id} {grid1x1.id} |',
+                  f'| {grid0x0.gid} {grid0x1.gid} |',
+                  f'| {grid1x0.gid} {grid1x1.gid} |',
                   '+-----+']
         self.assertEqual(lines, expect)
         self.assertEqual(grid0x0.location(), {'y': 0, 'x': 0})
         self.assertEqual(grid0x1.location(), {'y': 0, 'x': 1})
         self.assertEqual(grid1x0.location(), {'y': 1, 'x': 0})
         self.assertEqual(grid1x1.location(), {'y': 1, 'x': 1})
-        lines = str(layout).split("\n")
         expect = ['  ',
                   '  ']
-        self.assertEqual(lines, expect)
+        self.assertEqual(layout.lines, expect)
 
     def test_layout_2x2add4x1x1(self):
+        """This test demonstrates adding four text grids
+           and shows that updates to those text grids are
+           reflected on the stringified layout.
+        """
         layout = TextLayout(2, 2)
         grid0x0 = TextGrid(1, 1)
         grid0x1 = TextGrid(1, 1)
         grid1x0 = TextGrid(1, 1)
         grid1x1 = TextGrid(1, 1)
-        layout.add(grid0x0)
-        layout.add(grid0x1)
-        layout.add(grid1x0)
-        layout.add(grid1x1)
-        grid0x0.write('aa')
-        grid0x0.write('aa')
-        grid0x1.write('bb')
-        grid0x1.write('bb')
-        grid1x0.write('cc')
-        grid1x0.write('cc')
-        grid1x1.write('dd')
-        grid1x1.write('dd')
+        self.assertTrue(layout.add(grid0x0))
+        self.assertTrue(layout.add(grid0x1))
+        self.assertTrue(layout.add(grid1x0))
+        self.assertTrue(layout.add(grid1x1))
+        grid0x0.set(['aa', 'aa'])
+        grid0x1.set(['bb', 'bb'])
+        grid1x0.set(['cc', 'cc'])
+        grid1x1.set(['dd', 'dd'])
         lines = layout.reservations().split("\n")
         expect = ['+-----+',
-                  f'| {grid0x0.id} {grid0x1.id} |',
-                  f'| {grid1x0.id} {grid1x1.id} |',
+                  f'| {grid0x0.gid} {grid0x1.gid} |',
+                  f'| {grid1x0.gid} {grid1x1.gid} |',
                   '+-----+']
         self.assertEqual(lines, expect)
-        lines = str(layout).split("\n")
         expect = ['ab',
                   'cd']
-        self.assertEqual(lines, expect)
+        self.assertEqual(layout.lines, expect)
+
+    def test_layout_mixedgrids(self):
+        """This test demonstrates a layout with mixed grids with
+           borders and no borders.
+        """
+        layout = TextLayout(5, 5)
+        grid0x0 = TextGrid(3, 3, border=True)
+        grid0x3 = TextGrid(3, 2)
+        grid3x0 = TextGrid(2, 3)
+        grid3x3 = TextGrid(2, 2)
+        self.assertTrue(layout.add(grid0x0))
+        self.assertTrue(layout.add(grid0x3))
+        self.assertTrue(layout.add(grid3x0))
+        self.assertTrue(layout.add(grid3x3))
+        grid0x0.set(['a'])
+        grid0x3.set(['bb', 'bb', 'bb'])
+        grid3x0.set(['ccc', 'ccc'])
+        grid3x3.set(['dd', 'dd'])
+        expect = ['   bb',
+                  ' a bb',
+                  '   bb',
+                  'cccdd',
+                  'cccdd']
+        self.assertEqual(layout.lines, expect)
 
 
 if __name__ == '__main__':
--- textlayout/__init__.py
+++ textlayout/__init__.py
@@ -1,9 +1,29 @@
+"""
+    The TextLayout and TextGrid classes address the need to work with stirngs
+    in two dimentional grids, as opposed to the usual one dimensional string.
+
+    The TextGrid is a two dimentional string which is pre-populated with spaces
+    if it is uninitialized.
+
+    The TextLayout is a similar to the TextGrid in that it is a two dimentional
+    text object with fixed dimensions. However, the content of the TextLayout
+    object is derived from its component TextGrid instances which are written
+    two separately.
+
+    Author: Dylan Doxey <dylan.doxey@gmail.com>
+    Date: May 29, 2023
+"""
+
+
 class TextLayout:
-    """TextLayout
+    """TextLayout is a composition of TextGrid instances where the layout of
+       how the grids render is determined at the time they are added to the
+       layout.
     """
     def __init__(self, height, width, border=False):
         if (height < 3 or width < 3) and (height == 0 or width == 0 or border):
-            raise Exception(f'height: {height}, width: {width}, border: {border} -- invalid')
+            raise Exception(f'height: {height}, width: {width}, '
+                            f'border: {border} -- invalid')
         self.height = height
         self.width = width
         self.border = border
@@ -11,11 +31,20 @@ class TextLayout:
         self.data = {}
 
     def __str__(self):
+        return "\n".join(self.lines)
+
+    @property
+    def lines(self):
+        """The lines property provides a list of strings which represent the
+           content of the TextLayout as populated by its component TextGrid
+           instances.
+        """
         lines = []
-        b = ""
+        b_char = ""
         if self.border:
-            b = " "
-            lines.append('{}{}{}'.format(b, " " * self.width - 2, b))
+            b_char = " "
+            border = b_char * self.width
+            lines.append(border)
         for ypos, row in enumerate(self.reserved_):
             line = ""
             for xpos, grid_id in enumerate(row):
@@ -23,19 +52,24 @@ class TextLayout:
                     line += " "
                 else:
                     line += self.data[grid_id].read(ypos, xpos)
-            lines.append('{}{}{}'.format(b, line, b))
+            lines.append(f'{b_char}{line}{b_char}')
         if self.border:
             lines.append(lines[0])
-        return "\n".join(lines)
+        return lines
 
     def reservations(self):
+        """Produces a text representation of the grid reservations on the
+           layout. Useful for debugging.
+        """
         lines = ['+' + ('-' * (1 + self.width * 2)) + '+']
-        b = ""
+        b_char = ""
         if self.border:
-            b = " "
-            lines.append('|{} {} {}|'.format(b, " " * self.width, b))
+            b_char = " "
+            border = b_char * self.width
+            lines.append(border)
         for row in self.reserved_:
-            lines.append('|{} {} {}|'.format(b, " ".join([str(c) for c in row]), b))
+            line = " ".join([str(c) for c in row])
+            lines.append(f'|{b_char} {line} {b_char}|')
         if self.border:
             lines.append(lines[1])
         lines.append(lines[0])
@@ -87,70 +121,107 @@ class TextLayout:
         """Reserve a spot on the layout for the grid."""
         for row in range(grid.ypos, grid.ypos + grid.height):
             for col in range(grid.xpos, grid.xpos + grid.width):
-                if row < len(self.reserved_) and col < len(self.reserved_[row]):
+                if row < len(self.reserved_) and \
+                   col < len(self.reserved_[row]):
                     if self.reserved_[row][col] > 0:
-                        raise AssertionError(f'{row},{col} is already reserved')
-                    self.reserved_[row][col] = grid.id
-        self.data[grid.id] = grid
+                        raise AssertionError(f'{row},{col} already reserved')
+                    self.reserved_[row][col] = grid.gid
+        self.data[grid.gid] = grid
 
     def add(self, grid):
+        """Find a position on the layout for the given grid and assign the
+           position properties on the grid and retains a reference to the
+           grid so that updates to the grid content will be reflected when
+           the layout lines are rendered.
+        """
         position = self.search(grid)
         if position is not None:
-            grid.locate(position)
+            grid.locate(position['ypos'], position['xpos'])
             self.reserve(grid)
             return True
         return False
 
 
 class TextGrid(TextLayout):
-    id_ = 1
+    """The TextGrid is a two dimentional collection of characters."""
+    gid_ = 1
 
     def __init__(self, height, width, border=False):
         super().__init__(height, width, border)
         self.ypos = 0
         self.xpos = 0
-        self.lines = []
-        self.id = __class__.id_
-        __class__.id_ += 1
+        self.rows = []
+        self.gid = __class__.gid_
+        __class__.gid_ += 1
 
     def __str__(self):
-        text, b = "", ""
-        yend = self.ypos + self.height
-        xend = self.xpos + self.width
-        if self.border:
-            b = " "
-            yend -= 2
-            xend -= 2
-            text += b * self.width + "\n"
-        for ypos in range(self.ypos, yend):
-            text += b
-            for xpos in range(self.xpos, xend):
-                text += self.read(ypos, xpos)
-            text += f'{b}\n'
-        if self.border:
-            text += b * self.width + "\n"
+        text = ""
+        for row_i in range(self.height):
+            for col_i in range(self.width):
+                text += self.read(self.ypos + row_i, self.xpos + col_i)
+            text += '\n'
         return text.rstrip("\n")
 
-    def locate(self, position):
-        self.ypos = position['ypos']
-        self.xpos = position['xpos']
+    def locate(self, ypos, xpos):
+        """Set the x and y position attributes of the TextGrid."""
+        self.ypos = ypos
+        self.xpos = xpos
 
     def location(self):
+        """Get a dict with the x and y position attributes."""
         if self.ypos is None or self.xpos is None:
             return None
         return {'y': self.ypos, 'x': self.xpos}
 
     def write(self, line):
-        if len(self.lines) < self.height:
-            self.lines.append(line[0:self.width])
+        """Write a line to the grid. Any lines or characters that are
+           out of bounds will be discarded.
+        """
+        max_height, max_width = self.height, self.width
+        if self.border:
+            max_height -= 2
+            max_width -= 2
+        if len(self.rows) < max_height:
+            self.rows.append(line[0:max_width])
+            return True
+        return False
 
-    def read(self, y, x):
-        ypos = y - self.ypos
-        xpos = x - self.xpos
+    def set(self, text):
+        """Set the content of the grid. The source data can be a list of
+           strings, or a string with newlines, or an object that can be
+           stringified.
+        """
+        if text is None:
+            self.rows = []
+            return
+        lines = None
+        if isinstance(text, list):
+            lines = text
+        elif isinstance(text, str):
+            lines = text.split("\n")
+        else:
+            lines = str(text).split("\n")
+        for line in lines:
+            if not self.write(line):
+                break
+
+    def read(self, y_coord, x_coord):
+        """Read the character for the given x and y coordinates of the grid.
+           The x and y given are for the coordinates of the grid according to
+           its location in the layout.
+        """
+        if self.border:
+            if y_coord == 0 or y_coord == self.height - 1 or \
+               x_coord == 0 or x_coord == self.width - 1:
+                return " "
+        ypos = y_coord - self.ypos
+        xpos = x_coord - self.xpos
         if ypos >= self.height or xpos >= self.width:
             raise Exception(f'{ypos} >= {self.height}, '
                             f'{xpos} >= {self.width}: out of range')
-        if ypos < len(self.lines) and xpos < len(self.lines[ypos]):
-            return self.lines[ypos][xpos]
+        if self.border:
+            ypos -= 1
+            xpos -= 1
+        if ypos < len(self.rows) and xpos < len(self.rows[ypos]):
+            return self.rows[ypos][xpos]
         return " "
-
